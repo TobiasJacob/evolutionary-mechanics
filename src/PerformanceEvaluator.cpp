b1873a8a5e8e5e9e62d97c48a41787852625fc18
Tobias Jacob <tobi.jacob@gmx.net>
2020-11-15 19:24:21 -0600

    Discovered efficent algorithm for matrix generation

--- src/PerformanceEvaluator.cpp
+++ src/PerformanceEvaluator.cpp
@@ -1,7 +1,17 @@
 #include "PerformanceEvaluator.hpp"
 
+const static double K[6][6] = {
+    { 300,  180, -240,  -60,  -60, -120},
+    { 180,  300, -120,  -60,  -60, -240},
+    {-240, -120,  240,    0,    0,  120},
+    { -60,  -60,    0,   60,   60,    0},
+    { -60,  -60,    0,   60,   60,    0},
+    {-120, -240,  120,    0,    0,  240}
+};
+
 double PerformanceEvaluator::GetPerformance(Field &field)
 {
+    field.CalculateIndex();
     // Dummy implementation that returns the number of blocks
     double result = 0;
     for (int i = 0; i < field.Rows; i++)
