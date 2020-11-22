837d205ac793452668087d8311bbad0506772a4b
Raffaele Galliera <gallieraraffaele@gmail.com>
2020-11-22 17:44:46 -0600

    chore: update header

--- src/EvolutionaryOptimizator.hpp
+++ src/EvolutionaryOptimizator.hpp
@@ -11,53 +11,51 @@
 
 class EvolutionaryOptimizator
 {   
-
-    struct sorting_element{
-
-        unsigned int fitness;
-        int state;
-        struct sorting_element *nextElement;
-    };
-
-    typedef struct{
-
-        Field field;
-    }organism;
-
-    typedef struct{
-
-        //Initial Organisms, must be malloc'ed
-        organism *organisms;
-   
-        //number of organisms in the array
-        int organismCount;
-
-        //Tests organism's fitness to the problem
-        unsigned int (*fitnessTest) (organism org);
-
-        //Organigms lenght (?) not sure about this
-        int orgRow;
-        int orgCol;
-        // int orgDnaLength;
-
-        //How much we want to get to the perfect solution to our problem
-        //The EA will converge here
-        unsigned int desiredFitness;
-
-        //Heads-up about how close the organisms are
-        void(*progress) (unsigned int fitness);
-    }evolutionary_algorithm;
-
-    private:
-
     public:
+        typedef struct{
+
+            Field* field;
+            int rows, cols;
+        }organism;
+
+        typedef struct{
+
+            //Initial Organisms, must be malloc'ed
+            organism *organisms;
+   
+            //number of organisms in the array
+            int organismsCount;
+
+            //Tests organism's fitness to the problem
+            unsigned int (*fitnessTest) (organism org);
+
+            //Organigms lenght (?) not sure about this
+            int orgRows;
+            int orgCols;
+            // int orgDnaLength;
+
+            //How much we want to get to the perfect solution to our problem
+            //The EA will converge here
+            unsigned int desiredFitness;
+
+            //Heads-up about how close the organisms are
+            void(*progress) (unsigned int fitness);
+        }evolutionary_algorithm;
+
         EvolutionaryOptimizator();
-        EvolutionaryOptimizator(const Field&, const Support &supports, const vector<Force> forces);
+        EvolutionaryOptimizator(const Field &field, const Support &supports, const vector<Force> forces);
          //this evolves the group of organisms until they reach a satisfactory fitness level
         organism evolve(evolutionary_algorithm ea);
 
         //Reproduction using 2 organisms - crossing, then mutation
         organism reproduce(organism org1, organism org2, evolutionary_algorithm alg);
+
+    private:
+        const Support &supports;
+        const vector<Force> forces;
+        unsigned int fitnessTest(EvolutionaryOptimizator::organism orgToTest);
+
 };
 
 #endif
+
