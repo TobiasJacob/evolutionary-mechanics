fde3b39e6fb0c8945e538a59161dfac583156edf
Tobias Jacob <tobi.jacob@gmx.net>
2020-11-22 23:57:44 -0600

    Microtime format

--- src/Microtime.cpp
+++ src/Microtime.cpp
@@ -3,11 +3,9 @@
 
 #define TEST 0 /* Set to 0 except when testing the code or timer resolution */
 
-
 double get_microtime_resolution(void)
 {
   double time1, time2;
-
   time1 = microtime();
   do 
   {
@@ -17,8 +15,6 @@ double get_microtime_resolution(void)
   return time2-time1;
 }
 
-
-
 double microtime(void)
 {
   struct timeval t;
@@ -27,28 +23,3 @@ double microtime(void)
   
   return 1.0e6*t.tv_sec + (double) t.tv_usec;
 }
-
-
-
-#if TEST==1
-
-#include <stdio.h>
-
-int main(void)
-{
-  unsigned long i, n=1000000000;
-  double result = 0.0, time1, time2;
-
-  printf("Timer resolution = %g micro seconds\n", get_microtime_resolution());
-  
-  time1 = microtime();
-  for(i=1; i<n; i++)
-    result += 1.0/i;
-  time2 = microtime();
-  
-  printf("Result = %g\tTime taken = %g seconds\n", result, (time2-time1)/1.0e6);
-  
-  return 0;
-}
-
-#endif
