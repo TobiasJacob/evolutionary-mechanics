f1b711a75d062692744e6bc175d8348e515e0e6d
Tobias Jacob <tobi.jacob@gmx.net>
2020-12-06 14:38:19 -0600

    Created a job 224

---
+++ jobFiles/job224-2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --nodes=8
+#SBATCH --ntasks=112
+#SBATCH --cpus-per-task=2
+#SBATCH --time=00:10:00
+#SBATCH --job-name=hybrid
+cd $SLURM_SUBMIT_DIR
+
+# mpiifort -xHOST -O3 -qopenmp -mt_mpi hello_hybrid.f90 -o hello_hybrid.exe
+
+# set variable so task placement works as expected
+export I_MPI_JOB_RESPECT_PROCESS_PLACEMENT=0
+
+mpirun -print-rank-map -n $SLURM_NTASKS -genv \
+ OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK -genv I_MPI_PIN_DOMAIN=omp \
+ build/program 10 112 1000 0.999
+echo ""
+mpirun -print-rank-map -n $SLURM_NTASKS -genv \
+ OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK -genv I_MPI_PIN_DOMAIN=omp \
+ build/program 20 112 1000 0.999
+echo ""
+mpirun -print-rank-map -n $SLURM_NTASKS -genv \
+ OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK -genv I_MPI_PIN_DOMAIN=omp \
+ build/program 40 112 1000 0.999
