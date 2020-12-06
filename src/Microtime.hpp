a9effdb7105b94cecbbec6d9151d5d7b225db8fc
muddasar-ali <68741549+muddasar-ali@users.noreply.github.com>
2020-12-06 12:54:01 -0600

    Documentation updated

--- src/Microtime.hpp
+++ src/Microtime.hpp
@@ -1,9 +1,30 @@
+/**
+ * @file Microtime.hpp
+ * @author Ashok Srinivasan
+ * @brief the code of these functions is used to get the time values that will be used to calculate the Performance in time
+ * 
+ * @version 1.0
+ * @date 2020-12-06
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
 #ifndef _microtime_h_
 #define _microtime_h_
 
-extern "C" {
-  double    microtime(void); /* Time in micro-seconds */
-  double    get_microtime_resolution(void); /* Timer resolution in micro-seconds */
+extern "C" { 
+  /**
+   * @brief this function returns the actual time in micro-seconds
+   * 
+   * @return double 
+   */
+  double    microtime(void);
+  /**
+   * @brief this function returns the difference between two time instances using microtime
+   * 
+   * @return double 
+   */
+  double    get_microtime_resolution(void);
 }
 
 
