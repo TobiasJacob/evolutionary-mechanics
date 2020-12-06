ed0ce971f441d71bdb32f4b194461a920ba1af1d
Tobias Jacob <tobi.jacob@gmx.net>
2020-12-05 18:06:25 -0600

    Wrote testcase for bufferencode decode

--- src/test/Organism-test.cpp
+++ src/test/Organism-test.cpp
@@ -1,8 +1,24 @@
 #include "catch/catch.hpp"
 
-#include "../EvolutionaryOptimizator.hpp"
+#include "../Organism.hpp"
 
 TEST_CASE("OrganismEncodeAndDecode", "[Equation]")
 {
+    srand(0);
+    Organism orgA(10, 10);
+    Organism orgB(10, 10);
 
+    orgA.loss = 22321.32;
+    for (size_t r = 0; r < orgA.field->Rows; r++)
+        for (size_t c = 0; c < orgA.field->Cols; c++)
+            orgA.field->Plane(r, c) = rand(0) % 2;
+        
+    vector<byte> buffer(Organism::getSize(10, 10));
+    orgA.writeIntoBuffer(buffer.data());
+    orgB.readFromBuffer(buffer.data());
+
+    REQUIRE(orgA.loss == orgB.loss);
+    for (size_t r = 0; r < orgA.field->Rows; r++)
+        for (size_t c = 0; c < orgA.field->Cols; c++)
+            REQUIRE(orgA.field->Plane(r, c) == orgB.field->Plane(r, c));
 }
